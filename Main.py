b0a8b481a4a7cd6691d354cab32e55bea9157f99
Anderson Sena <45375546+senadevolta@users.noreply.github.com>
2021-12-17 11:35:37 -0300

    UpDate Codigo

--- Main.py
+++ Main.py
@@ -2,89 +2,85 @@ import random
 import time
 import os
 import sys
+import collections
 
-
-def func_Escrever_Texto(texto):
+def func_texto_print(texto):
     for letra in texto:
         sys.stdout.write(letra)
         sys.stdout.flush()
         time.sleep(0.004)
 
+def func_menu_info():
+    print("\033[40;156;60m \t\t\t*** Menu do Game ***")
+    print(""
+        "((Bem vindo ao jogo adivinhe o número.))\n\033[m"
+        "Acabei de pensar em um número entre 1 e 100. "
+        "Tente descobrir esse número.\n"
+        "Digite EXIT para parar de jogar.",
+    )
+    time.sleep(1)
+    texto="..................................\n"
+    func_texto_print(texto)
 
-def func_menu_principal():
-    print("\033[44;30;10m :*** Menu do Game ***\033[m")
-    time.sleep(0.5)
-    texto = "São essas as opções...\n"
-    func_Escrever_Texto(texto)
-    texto = "Opção 01 - Mentalista \n"
-    func_Escrever_Texto(texto)
+def func_new_game():
+    jogar_novamente = input("Jogar novamente? ")
 
+    if jogar_novamente.strip().lower() in ["s", "si", "sim"]:
+        return
 
-def func_menu_Jogo():
-    opcao = int(input("Escolha:  "))
-    if (opcao == 1):
-        print('OPÇÃO JOGO DO MENTALISTA')
-        x = func_randomizador()
-        numero = 0
-        func_mentalista(x, numero)
-    elif (opcao == 2):
-        print('OPÇÃO INVALIDA')
-        func_menu_Jogo(x, numero)
+    print("Até a próxima!")
+    sys.exit()
+
+def func_new_info():
+    print(
+        "Legal! Acabei de pensar em outro número entre 1 e 100.\n"
+        "Tente adivinhar, ou digite sair para finalizar o jogo.\n"
+    )
+
+def func_exit(opcao):
+    if opcao.strip().lower() == "exit":
+        print("\033[7;30;90m *** !OBRIGADO POR JOGAR, ATÉ UMA PROXIMA! ***\033[m")
+        sys.exit()
+
+def func_str_number(opcao):
+    try:
+        opcao = int(opcao)
+        return opcao
+    except ValueError:
+        return None
+
+def checar_palpite_correto(opcao, número_aleatório):
+    if opcao > número_aleatório:
+        ('O numero {} e Maior que o numero pensado..'.format(opcao))
+    elif opcao < número_aleatório:
+        print('O numero {} e Menor que o numero pensado..'.format(opcao))
     else:
-        func_menu_principal()
-        func_menu_Jogo()
+        print("Parabéns! Você acertou!\n")
+        return True
+    return False
 
+def func_mentalista():
+    func_menu_info()
+    número_aleatório = random.randint(1, 100)
 
-def func_titulo():
-    print('(((      Bem vindo ao Gamer da Sorte     )))')
-    texto = ('Pressione ENTER........................:')
-    func_Escrever_Texto(texto)
-    input()
-    texto = "Iremos começar...\n"
-    func_Escrever_Texto(texto)
-
-
-def func_randomizador():
-    numero_gerado = random.randint(1, 100)
-    return numero_gerado
-
-
-def func_mentalista(x, numero):
     while True:
-        chute = int(input('Chute um numero de 0 a 100: '))
-        if (chute > 100 or chute < 0):
-            print("OPÇÃO INVALIDA..")
-        elif (chute > x):
-            print('Numero pensado: {}'.format(x))
-            print('O numero {} que você digitou e Maior que o numero pensado..'.format(chute))
-        elif (chute < x):
-            print('Numero pensado: {}'.format(x))
-            print('O numero {} que você digitou e Menor que o numero pensado..'.format(chute))
-        elif (chute == x):
-            print('Numero pensado: {}'.format(x))
-            print('Parabéns.. você acertou.. ')
-            print('O numero pensado foi {}'.format(chute))
-            opcao = str(input('Ainda deseja jogar....(S/N): '))
-            if opcao.lower() == 's':
-                print('Parabéns.. Reiniciando jogo..')
-                time.sleep(2)
-                x = func_randomizador()
-                numero = 0
-                func_mentalista(x, numero)
-                return ()
-            elif opcao.lower() == 'n':
-                texto = "(S) Exit, (M) Menu..."
-                func_Escrever_Texto(texto)
-                xcod = str(input("..: ")).lower()
-                if (xcod == 's'):
-                    print('Parabéns, Finalizando o Jogo!!..')
-                    sys.exit()
-                    break
-                if (xcod == 'm'):
-                    x = func_randomizador()
-                    numero = 0
-                    func_mentalista(x, numero)
+        opcao = input("Digite um número entre 1 e 100: ")
+        func_exit(opcao)
 
-func_titulo()
-func_menu_principal()
-func_menu_Jogo()
+        opcao = func_str_number(opcao)
+        if opcao is None:
+            print("Erro: Você deve digitar um número! Tente novamente.\n")
+            continue
+
+        if 1 <= opcao <= 100:
+           opcao_correta = checar_palpite_correto(opcao, número_aleatório)
+        else:
+            print("Erro: O número deve estar entre 1 e 100! Tente novamente.\n")
+            continue
+
+        if opcao_correta:
+            func_new_game()
+            func_new_info()
+            número_aleatório = random.randint(1, 100)
+
+func_mentalista()
